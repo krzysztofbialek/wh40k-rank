936c6bccd42faae24a2482f14300aa3d6071fcd5
krzysztofbialek <zlo@o2.pl>
2014-10-18 12:29:09 +0200

    generate model for user and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  pots '/login', 'sessions#create'
+  delete '/loggout', 'sessions#destroy'
+
+  get 'sessions/destroy'
+
   get 'tournaments/index'
 
   root "dashboard#index"
