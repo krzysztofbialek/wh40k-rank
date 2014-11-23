a8717d8cca84f4cb2a8dadae0176984ec69f8d00
krzysztofbialek <krzysztof.bialek@gmail.com>
2014-11-23 21:47:32 +0100

    handle session in initializer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,8 @@
 //= require ember
 //= require ember-data
 //= require_self
+//= require mixins/current_user_proxy
+//= require initializers/current_user_initializer
 //= require ./wh40k_rank
 
 Wh40kRank = Ember.Application.create({
@@ -16,23 +18,5 @@ Wh40kRank = Ember.Application.create({
 
 });
 
-Ember.Application.initializer({
-  name: 'currentUser',
-  initialize: function(container, application) {
-    application.deferReadiness();
-    proxy = Ember.Object.extend({signedIn: false})
-    application.register('user:current', proxy, { singleton: true } );
 
-    Ember.$.get('/logged_in', function() {
-      console.log('hodor');
-      container.lookup('user:current').set('signedIn', true)
-    }).fail(function() {
-      container.lookup('user:current').set('signedIn', false)
-    });
-
-    application.inject('controller', 'currentUser', 'user:current')
-    application.inject('route', 'currentUser', 'user:current')
-    application.advanceReadiness();
-  }
-});
 
--- app/assets/javascripts/initializers/current_user_initializer.js
+++ app/assets/javascripts/initializers/current_user_initializer.js
@@ -2,19 +2,18 @@ Ember.Application.initializer({
   name: 'currentUser',
   initialize: function(container, application) {
     application.deferReadiness();
-    proxy = CurrentUserProxy.extend()
-    console.log('Hodor')
-    application.register 'user:current', proxy, singleton: true  }
+    proxy = Ember.Object.extend({signedIn: false})
+    application.register('user:current', proxy, { singleton: true } );
 
     Ember.$.get('/logged_in', function() {
-      container.lookup('user:current').set 'signedIn', true
+      console.log('hodor');
+      container.lookup('user:current').set('signedIn', true)
     }).fail(function() {
-      container.lookup('user:current').set 'signedIn', false
+      container.lookup('user:current').set('signedIn', false)
     });
 
-    application.inject 'controller', 'currentUser', 'user:current'
-    application.inject 'route', 'currentUser', 'user:current'
-    console.log(proxy)
-    application.advanceReadiness()
-  };
+    application.inject('controller', 'currentUser', 'user:current')
+    application.inject('route', 'currentUser', 'user:current')
+    application.advanceReadiness();
+  }
 });
